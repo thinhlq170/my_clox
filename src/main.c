49815081129d793138aabdc99f743a025f9c2ea7
ltq4hc <thinh.lequang2@vn.bosch.com>
2024-10-01 10:23:45 +0700

    [chunk_of_bytecode] Complete Disassembling Chunks 14.4

--- src/main.c
+++ src/main.c
@@ -1,10 +1,14 @@
 #include "../inc/common.h"
 #include "../inc/chunk.h"
+#include "..inc/debug.h"
 
 int main(int argc, const char *argv[]) {
     Chunk chunk;
     initChunk(&chunk);
     writeChunk(&chunk, OP_RETURN);
+
+    disassembleChunk(&chunk, "test chunk");
+
     freeChunk(&chunk);
 
     return 0;
